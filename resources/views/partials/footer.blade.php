9e24c46f6d979094ca87c565fbf081d7939936ba
noprizal <rizalnov667@gmail.com>
2025-03-09 00:29:43 +0700

    setup baldes

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="bg-[#2A4D7D] text-white py-4 mt-auto">
+    <div class="container mx-auto text-center">
+        <p>
+            &copy; 2025 -
+            <a href="https://instagram.com/kkbwk" target="_blank"">
+                target="_blank" class="text-blue-400 hover:underline">
+                Noprizal
+            </a>
+        </p>
+    </div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ContactController;
+
+// Redirect the root URL to the contacts index page
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('contacts.index');
 });
+
+// Define a resource route for the ContactController
+// This automatically sets up standard routes for index, create, store, show, edit, update, and destroy actions
+Route::resource('contacts', ContactController::class);
